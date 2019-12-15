928f43b2b1fc9e50c87e92d9283a0ad1dcee2d9a
Mark van Wijk <m.g.w.vanwijk@student.utwente.nl>
2019-12-15 20:23:49 +0100

    split day1 into parts

--- day01/fuel.py
+++ day01/fuel.py
@@ -1,19 +1,28 @@
-def calculate_fuel(load: [int]) -> int:
-    return sum([calculate_part_fuel(mass) for mass in load])
-
-
 def calculate_part_fuel(mass: int) -> int:
-    partial_fuel: int = int(mass / 3) - 2
-    if partial_fuel > 0:
-        return partial_fuel + calculate_part_fuel(partial_fuel)
-    else:
-        return 0
+    partial_fuel: int = mass // 3 - 2
+    return partial_fuel + calculate_part_fuel(partial_fuel) if partial_fuel > 0 else 0
 
 
-with open('input.txt') as file:
+def get_input():
     total_masses = []
-    line = file.readline()
-    while line:
-        total_masses.append(int(line))
+    with open('input.txt') as file:
         line = file.readline()
-    print(calculate_fuel(total_masses))
+        while line:
+            total_masses.append(int(line))
+            line = file.readline()
+    return total_masses
+
+
+def part1():
+    fuel = sum([mass // 3 - 2 for mass in get_input()])
+    print(f'Fuel sum: {fuel}')
+
+
+def part2():
+    fuel = sum([calculate_part_fuel(mass) for mass in get_input()])
+    print(f'Total fuel: {fuel}')
+
+
+if __name__ == '__main__':
+    part1()
+    part2()
