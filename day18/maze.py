78f780be0fee007faf65e13b5c2c0e4f5d1dbc91
Mark van Wijk <m.g.w.vanwijk@student.utwente.nl>
2019-12-21 14:54:43 +0100

    clean up day 18 files

--- day18/maze.py
+++ day18/maze.py
@@ -1,97 +1,300 @@
-from day18.new import Maze, read_maze_from_file
+class Line:
+
+    def __init__(self, length: int, start: (int, int), end: (int, int), doors: {(int, int)}):
+        self.length = length
+        self.start = start
+        self.end = end
+        self.doors = doors
+
+    def __len__(self):
+        return self.length
+
+    def __eq__(self, other):
+        return isinstance(other, Line) and (self.start == other.end or self.start == other.start) \
+               and (self.end == other.end or self.end == other.start)
+
+    def __str__(self):
+        return f'<start={self.start} end={self.end} length={self.length}>'
 
 
-class MazeWalker:
+class Maze:
     ACTIONS = {(1, 0), (0, 1), (-1, 0), (0, -1)}
 
-    def __init__(self, maze: Maze, moves: int = 0, found_keys: {(str, ...): int} = None,
-                 last_found: (str, ...) = (), session_locations=None):
-        self.maze = maze
-        self.moves = moves
-        self.last_found = last_found
-        self.found_keys = found_keys if found_keys else {}
-        self.session_locations = session_locations if session_locations else set()
+    def __init__(self, walls=None, keys=None, doors=None, cursor=None):
+        self.walls: {(int, int)} = walls if walls else set()
+        self.keys: {(int, int): str} = keys if keys else {}
+        self.doors: {(int, int): str} = doors if doors else {}
+        self.cursor = cursor if cursor else (0, 0)
+        self.lines: {(int, int): {Line}} = {}
 
-    def get_next(self) -> {'MazeWalker'}:
-        next_walkers = set()
-        cursor = self.maze.cursor
-        for action in self.ACTIONS:
-            next_cursor = cursor[0] + action[0], cursor[1] + action[1]
-            if next_cursor not in self.maze.walls \
-                    and next_cursor not in self.session_locations \
-                    and next_cursor not in self.maze.doors:
+    def obtain_key(self, location: (int, int)):
+        name = self.keys[location].upper()
+        self.cursor = location
+        del self.keys[location]
+        for door in self.doors.copy():
+            if self.doors[door] == name:
+                del self.doors[door]
 
-                next_walker = self.copy()
-                next_walker.maze.cursor = next_cursor
-                next_walker.moves += 1
+    def _generate_lines(self, point, critical_points):
+        paths = [(point, point, set(), 0)]
+        result = []
 
-                if next_cursor in self.maze.keys:
-                    next_walker.session_locations = set()
-                    door_name = next_walker.maze.keys[next_cursor].upper()
+        while len(paths) > 0:
+            temp_paths = paths.copy()
+            paths = []
+            for path in temp_paths:
+                for action in self.ACTIONS:
+                    target = path[0][0] + action[0], path[0][1] + action[1]
+                    if target not in self.walls and target != path[1]:
+                        doors = path[2].copy()
+                        if target in self.doors:
+                            doors.add(target)
 
-                    current_keys = list(self.last_found)
-                    current_keys.append(door_name.lower())
-                    self.last_found = tuple(current_keys)
-                    self.found_keys[self.last_found] = self.moves
+                        if target in critical_points:
+                            result.append(Line(path[3] + 1, point, target, doors))
+                        else:
+                            paths.append((target, path[0], doors, path[3] + 1))
+        return result
 
-                    del next_walker.maze.keys[next_cursor]
-                    doors = next_walker.maze.doors
-                    for door_loc in doors.copy():
-                        if doors[door_loc] == door_name:
-                            del doors[door_loc]
-                            break
+    def render(self):
+        intersections = self.get_splits()
+        for y in range(max(coord[1] for coord in self.walls) + 1):
+            line = ''
+            for x in range(max(coord[0] for coord in self.walls) + 1):
+                if (x, y) in intersections:
+                    line += '&'
+                elif (x, y) in self.walls:
+                    line += '#'
+                elif (x, y) in self.keys:
+                    line += 'k'
+                elif (x, y) in self.doors:
+                    line += 'D'
                 else:
-                    next_walker.session_locations.add(cursor)
+                    line += '.'
+            print(line)
 
-                next_walkers.add(next_walker)
-        return next_walkers
+    def generate_lines(self):
+        critical_points = self.get_splits()
+        critical_points.update(self.keys)
+        critical_points.add(self.cursor)
+        for critical_point in critical_points:
+            self.lines[critical_point] = self._generate_lines(critical_point, critical_points)
 
-    def matches_found(self, found: {(str, ...): (int, 'MazeWalker')}) -> bool:
-        if len(self.last_found) > 0:
-            for target in self.found_keys:
-                if target in found:
-                    value = self.found_keys[target]
-                    if found[target] > value:
-                        found[target] = value
-                        return True
-                    elif found[target] == value:
-                        return True
-                    else:
-                        return False
-                else:
-                    found[target] = self.found_keys[target]
-                    return True
-        else:
-            return True
+    def get_path_sides(self, path_criteria, final_criteria) -> {(int, int)}:
+        result = set()
+        for y in range(max(coord[1] for coord in self.walls)):
+            for x in range(max(coord[0] for coord in self.walls)):
+                path_count = 0
+                if (x, y) not in self.walls:
+                    for action in self.ACTIONS:
+                        if path_criteria((x + action[0], y + action[1])):
+                            path_count += 1
+                    if final_criteria(path_count):
+                        result.add((x, y))
+        return result
 
-    def is_finished(self):
-        return len(self.maze.keys) == 0
+    def get_splits(self) -> {(int, int)}:
+        return self.get_path_sides(lambda coord: coord not in self.walls, lambda count: count > 2)
+
+    def get_dead_ends(self) -> {(int, int)}:
+        return self.get_path_sides(lambda coord: coord not in self.walls, lambda count: count == 1)
 
     def copy(self):
-        return MazeWalker(self.maze.copy(), self.moves, self.found_keys.copy(),
-                          self.last_found, self.session_locations.copy())
+        return Maze(self.walls.copy(), self.keys.copy(), self.doors.copy(), self.cursor)
 
 
-def solve_maze(maze: Maze):
-    found: {(str, ...): (int, MazeWalker)} = {}
-    working: {MazeWalker} = {MazeWalker(maze)}
+class Path:
 
-    while len(working) > 0:
-        temp_working = working.copy()
-        working = set()
-        for target in temp_working:
-            if not target.is_finished():
-                for next_target in target.get_next():
-                    if next_target.matches_found(found):
-                        working.add(next_target)
-            else:
-                return target.moves
+    def __init__(self, maze: Maze, history: {Line}, start: (int, int), cursor: (int, int),
+                 end: (int, int)):
+        self.maze = maze
+        self.history = history
+        self.start = start
+        self.cursor = cursor
+        self.end = end
+
+    def get_next(self) -> {'Path'}:
+        result = set()
+
+        for line in self.maze.lines[self.cursor]:
+            if line not in self.history:
+                history = self.history.copy()
+                history.append(line)
+                result.add(Path(self.maze, history, self.start, line.end, self.end))
+        return result
+
+    def get_dependencies(self):
+        return {door for line in self.history for door in line.doors}
+
+    def is_done(self) -> bool:
+        return self.cursor == self.end
+
+    def __len__(self):
+        return sum(len(line) for line in self.history)
+
+    def __str__(self):
+        return f'start={self.start}, end={self.end}, cursor={self.cursor}, ' \
+               f'history={",".join([str(line) for line in self.history])}'
+
+
+class Solver:
+
+    def __init__(self, maze: Maze, history: [Path], key_paths: {(int, int): {(int, int): Path}}):
+        self.maze = maze
+        self.history = history
+        self.key_paths = key_paths
+
+    def get_next(self) -> {'Solver'}:
+        result = set()
+        for reachable in self.get_reachable():
+            next_maze = self.maze.copy()
+            next_maze.obtain_key(reachable.end)
+            history = self.history.copy()
+            history.append(reachable)
+            result.add(Solver(next_maze, history, self.key_paths))
+        return result
+
+    def get_reachable(self):
+        result = set()
+        for target in self.maze.keys:
+            if len(self.key_paths[self.maze.cursor][target].get_dependencies().intersection(
+                    {key for key in self.maze.doors})) == 0:
+                result.add(self.key_paths[self.maze.cursor][target])
+        return result
+
+    def is_solved(self):
+        return len(self.maze.keys) == 0
+
+    def get_covered(self) -> {(((int, int), ...), (int, int)): (int, [(int, int)])}:
+        covered = {}
+        history_places = [line.end for line in self.history]
+        for i in range(len(history_places)):
+            covered[(tuple(sorted(history_places[:i])), history_places[i])] = \
+                (sum(map(len, self.history[:i + 1])), self.history[:i + 1])
+        return covered
+
+    def __len__(self):
+        return sum(map(len, self.history))
+
+    def __str__(self):
+        return ', '.join([str(len(line)) for line in self.history]) + " :: " + ', '.join(
+            [str(line.start) for line in self.history])
+
+
+def get_path(maze: Maze, start: (int, int), end: (int, int)) -> Path:
+    paths: {int: {Path}} = {0: {Path(maze, [], start, start, end)}}
+    best_path = None
+    best_length = 0
+    while len(paths) > 0 and (not best_length or best_length < min(paths)):
+        length = min(paths)
+        targets = paths[length]
+        del paths[length]
+
+        for target in targets:
+            for next_target in target.get_next():
+                next_length = len(next_target)
+                if not next_target.is_done():
+                    if next_length in paths:
+                        paths[next_length].add(next_target)
+                    else:
+                        paths[next_length] = {next_target}
+                else:
+                    if not best_length or best_length > next_length:
+                        best_length = next_length
+                        best_path = next_target
+    return best_path
+
+
+def read_maze_from_file() -> Maze:
+    maze: Maze = Maze()
+    y = 0
+    with open('input.txt') as file:
+        for line in file.readlines():
+            line = line.strip()
+            for x in range(len(line)):
+                if line[x] == '#':
+                    maze.walls.add((x, y))
+                elif line[x] == '@':
+                    maze.cursor = (x, y)
+                elif line[x].upper() != line[x].lower() == line[x]:
+                    maze.keys[(x, y)] = line[x]
+                elif line[x].lower() != line[x].upper() == line[x]:
+                    maze.doors[(x, y)] = line[x]
+            y += 1
+    print(maze.doors)
+    print(maze.keys)
+
+    return maze
+
+
+def get_key_paths(maze: Maze) -> {(int, int): {(int, int): Path}}:
+    paths = {}
+    for start_coord in maze.keys:
+        if maze.cursor in paths:
+            paths[maze.cursor][start_coord] = get_path(maze, maze.cursor, start_coord)
+        else:
+            paths[maze.cursor] = {start_coord: get_path(maze, maze.cursor, start_coord)}
+
+        for end_coord in maze.keys:
+            if start_coord != end_coord:
+                path = get_path(maze, start_coord, end_coord)
+
+                if start_coord in paths:
+                    paths[start_coord][end_coord] = path
+                else:
+                    paths[start_coord] = {end_coord: path}
+
+    return paths
+
+
+def shortest_path(maze: Maze) -> Solver:
+    solvers: {int: {Solver}} = {0: {Solver(maze, [], get_key_paths(maze))}}
+    best_paths: {(((int, int), ...), (int, int)): (int, [(int, int)])} = {}
+    best_length = 0
+    best_route = 0
+
+    while len(solvers) > 0 and (not best_length or best_length > min(solvers)):
+        target_length = min(solvers)
+        targets = solvers[target_length]
+        del solvers[target_length]
+        for target in targets:
+            for next_target in target.get_next():
+                target_length = len(next_target)
+                if not next_target.is_solved():
+                    covered = next_target.get_covered()
+                    better_alternative = False
+                    for piece in covered:
+                        if piece in best_paths:
+                            if best_paths[piece][0] < covered[piece][0]:
+                                better_alternative = True
+                                break
+                            elif best_paths[piece][0] > covered[piece][0]:
+                                best_paths[piece] = covered[piece]
+                            else:
+                                if best_paths[piece][1] != covered[piece][1]:
+                                    better_alternative = True
+                                    break
+                        else:
+                            best_paths[piece] = (covered[piece])
+
+                    if not better_alternative:
+                        if target_length not in solvers:
+                            solvers[target_length] = {next_target}
+                        else:
+                            solvers[target_length].add(next_target)
+                else:
+                    if not best_length or target_length < best_length:
+                        best_length = target_length
+                        best_route = next_target
+
+    return best_route
 
 
 def part1():
     maze: Maze = read_maze_from_file()
-    print(maze.get_splits())
-    print(maze.get_dead_ends())
+    maze.generate_lines()
+    path = shortest_path(maze)
+    print(f'Shortest path: {len(path)}')
 
 
 def part2():
